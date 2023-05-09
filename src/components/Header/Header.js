2c025e8e8c004bc78bce3c228b7b4836484667be
Gerik Castillo <geriklc@gmail.com>
2023-05-08 23:39:45 -0400

    added additional nav bar and extra colors. Initialized the font style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,20 @@
 import "./Header.scss";
 import SearchIcon from "../../assets/icons/search.svg";
 import { Link } from "react-router-dom";
-
+import Logo from "../../assets/logo/exposhare-logo.png"
+import Favorites from "../../assets/icons/likes.svg"
 
 function Header() {
   return (
     <header className="header-container">
       <div className="header">
-        <Link to ="/" className="header__logo">
-          exposhare
+        <Link to ="/">
+        <img
+            className="header__logo"
+            src={Logo}
+            alt="exposhare logo"
+    
+          />
         </Link>
         <nav className="header__nav-bar">
             <form className="header__label">
@@ -23,10 +29,19 @@ function Header() {
                 placeholder="Search"
               ></input>
             </form>
-            <img></img>
+            <div className="header__wrapper">
+            <img src={Favorites} alt="favorites" className="header__likes-icon"></img>
             <div className="header__avatar"></div>
+            </div>
         </nav>
       </div>
+      <nav className="nav">
+        <a href="#" className="nav__link nav__link--current">Shop</a>
+        <a href="#" className="nav__link">Sell</a>
+        <a href="#" className="nav__link">Trade</a>
+        <a href="#" className="nav__link">Repair</a>
+        <a href="#" className="nav__link">News</a>
+      </nav>
     </header>
   );
 }
