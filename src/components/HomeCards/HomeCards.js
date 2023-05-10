9c27576374516d9c49409a801456319465bdb467
Gerik Castillo <geriklc@gmail.com>
2023-05-09 23:30:07 -0400

    initialized homecards folder and files

---
+++ src/components/HomeCards/HomeCards.js
@@ -0,0 +1,21 @@
+import "./HomeCards.scss";
+
+function HomeCards() {
+    
+    return (
+        <>
+      <section className="card-category">
+        
+      </section>
+      <section className="card-category">
+        
+      </section>
+      <section className="card-services">
+        
+      </section>
+      </>
+    );
+  }
+  
+  export default HomeCards;
+  
