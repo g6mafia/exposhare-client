4728560b4282ee3d9636c69fe979ee46256838da
Gerik Castillo <geriklc@gmail.com>
2023-05-09 20:12:26 -0400

    further styling to footer at mobile view. Removed otf font type for Lexend

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,59 +4,98 @@ import Logo from "../../assets/logo/exposhare-logo.png";
 import FacebookIcon from "../../assets/icons/facebook.svg";
 import TwitterIcon from "../../assets/icons/twitter.svg";
 import InstaIcon from "../../assets/icons/instagram.svg";
+import ArrowRight from "../../assets/icons/arrow-right.svg"
 
 function Footer() {
   return (
-    <>
-      <footer className="footer-container">
-        <section className="footer">
-          <Link to="/">
-            <img className="footer__logo" src={Logo} alt="exposhare logo" />
-          </Link>
-          <div className="footer-wrapper"></div>
-        </section>
+    <div className="footer-container">
+      <footer className="footer">
+        <Link to="/">
+          <img className="footer__logo" src={Logo} alt="exposhare logo" />
+        </Link>
 
         <section className="footer__block-1">
-          <div className="footer__divider footer__divider-1">
-            <a href="#" className="footer__subtitle">
+          <div className="footer__divider">
+            <p href="#" className="footer__subtitle">
               ABOUT
-            </a>
+            </p>
+            <div className="footer__wrapper">
+              <a href="" className="footer__link">
+                Our Story
+              </a>
+              <a href="" className="footer__link">
+                FAQ
+              </a>
+              <a href="" className="footer__link">
+                TEAM
+              </a>
+              <a href="" className="footer__link">
+                Careers
+              </a>
+            </div>
           </div>
-          <div className="footer__divider footer__divider-2">
-            <a href="#" className="footer__subtitle">
-              FAQ
-            </a>
+          <div className="footer__divider">
+            <p href="#" className="footer__subtitle">
+              SERVICES
+            </p>
+            <div className="footer__wrapper">
+              <a href="" className="footer__link">
+                Shop
+              </a>
+              <a href="" className="footer__link">
+                Sell
+              </a>
+              <a href="" className="footer__link">
+                Trade
+              </a>
+              <a href="" className="footer__link">
+                Repair
+              </a>
+            </div>
           </div>
-          <div className="footer__divider footer__divider-3">
-            <a href="#" className="footer__subtitle">
-              PRIVACY POLICY
-            </a>
+          <div className="footer__divider">
+            <p href="#" className="footer__subtitle">
+              LEGAL
+            </p>
+            <div className="footer__wrapper">
+              <a href="" className="footer__link">
+                Terms & Conditions
+              </a>
+              <a href="" className="footer__link">
+                Privacy Policy
+              </a>
+              <a href="" className="footer__link">
+                Terms of Use
+              </a>
+            </div>
           </div>
-          <div className="footer__divider footer__divider-3">
-            <a href="#" className="footer__subtitle">
-              TERMS OF SERVICE
-            </a>
-          </div>
-          <div className="footer__divider footer__divider-3">
-            <a href="#" className="footer__subtitle">
-              PRIVACY
-            </a>
+
+          <div className="footer__divider">
+            <p href="#" className="footer__subtitle">
+              STAY CONNECTED
+            </p>
+            <form className="footer__form">
+              <label htmlFor="name" className="footer__label">
+                <input type="email" name="email" className="footer__input" placeholder="Enter your email address"></input>
+              </label>
+              <button type="submit" className="footer__button"><img src={ArrowRight} className="footer__icon footer__icon--size"/></button>
+            </form>
           </div>
         </section>
 
         <section className="footer__block-2">
           <a href="#" target="_blank">
-            <img src={FacebookIcon}></img>
+            <img src={FacebookIcon} className="footer__icon"></img>
           </a>
           <a href="#" target="_blank">
-            <img src={TwitterIcon}></img>
+            <img src={TwitterIcon} className="footer__icon"></img>
           </a>
           <a href="#" target="_blank">
-            <img src={InstaIcon}></img>
+            <img src={InstaIcon} className="footer__icon"></img>
           </a>
         </section>
       </footer>
-    </>
+    </div>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,18 @@
 import "./Header.scss";
 import SearchIcon from "../../assets/icons/search.svg";
 import { Link } from "react-router-dom";
-import Logo from "../../assets/logo/exposhare-logo.png"
-import Favorites from "../../assets/icons/likes.svg"
+import Logo from "../../assets/logo/exposhare-logo.png";
+import Favorites from "../../assets/icons/likes.svg";
 
 function Header() {
   return (
     <>
-    <header className="header-container">
-      <div className="header">
-        <Link to ="/">
-        <img
-            className="header__logo"
-            src={Logo}
-            alt="exposhare logo"
-    
-          />
-        </Link>
-        <nav className="header__nav-bar">
+      <header className="header-container">
+        <div className="header">
+          <Link to="/">
+            <img className="header__logo" src={Logo} alt="exposhare logo" />
+          </Link>
+          <nav className="header__nav-bar">
             <form className="header__label">
               <img
                 className="header__search-icon"
@@ -29,24 +24,41 @@ function Header() {
                 className="header__search header__search--active"
                 placeholder="Search"
               ></input>
+              <button>SEARCH</button>
             </form>
             <div className="header__wrapper">
-            <Link to ="/favorites"><img src={Favorites} alt="favorites" className="header__likes-icon"></img></Link>
-            <div className="header__avatar"></div>
+              <Link to="/favorites">
+                <img
+                  src={Favorites}
+                  alt="favorites"
+                  className="header__likes-icon"
+                ></img>
+              </Link>
+              <div className="header__avatar"></div>
             </div>
-        </nav>
-      </div>
+          </nav>
+        </div>
       </header>
       <section className="nav-container">
-      <nav className="nav">
-        <a href="#" className="nav__link nav__link--current">Shop</a>
-        <a href="#" className="nav__link">Sell</a>
-        <a href="#" className="nav__link">Trade</a>
-        <a href="#" className="nav__link">Forum</a>
-        <a href="#" className="nav__link">News</a>
-      </nav>
+        <nav className="nav">
+          <a href="#" className="nav__link nav__link--current">
+            SHOP
+          </a>
+          <a href="#" className="nav__link">
+            SELL
+          </a>
+          <a href="#" className="nav__link">
+            TRADE
+          </a>
+          <a href="#" className="nav__link">
+            FORUM
+          </a>
+          <a href="#" className="nav__link">
+            NEWS
+          </a>
+        </nav>
       </section>
-      </>
+    </>
   );
 }
 
