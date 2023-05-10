fb452a2f1885a4d64f3ceaefa540ff8deffbbcfc
Gerik Castillo <geriklc@gmail.com>
2023-05-09 21:53:57 -0400

    added mixin for component spacing

---
+++ src/components/HeroBanner/HeroBanner.js
@@ -0,0 +1,12 @@
+import "./HeroBanner.scss";
+
+function HeroBanner() {
+    return (
+      <section className="hero-banner">
+        <h1 className="hero-banner__title" >exposhare</h1>
+        <p className="hero-banner__slogan">"Capture Your World: Discover the Perfect Gear for Every Shot"</p>
+      </section>
+    );
+  };
+  
+  export default HeroBanner;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,10 @@
-
+import HeroBanner from "../../components/HeroBanner/HeroBanner";
 
 function HomePage() {
     return (
       <section>
-        
+        <HeroBanner />
+        {/* card */}
       </section>
     );
   };
